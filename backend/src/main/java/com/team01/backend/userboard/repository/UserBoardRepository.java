2b71321a298edfea62561e3b310377daa0c0a211
c1oud-dev <gksmf0187@gmail.com>
2026-04-13 14:14:01 +0900

    remove: UserBoard 패키지 삭제
    - 관리자 담당자가 작성한 Board 패키지 사용으로 결정
    - UserBoard 관련 파일 전체 삭제
    - BoardController는 일반/관리자용으로 분리 예정

--- backend/src/main/java/com/team01/backend/userboard/repository/UserBoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team01.backend.userboard.repository;
-
-import com.team01.backend.userboard.entity.UserBoard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserBoardRepository extends JpaRepository<UserBoard, Long> {
-}
