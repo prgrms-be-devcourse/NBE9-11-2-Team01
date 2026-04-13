48483fa53b3e890ee9a5dc2b5eb9a361b2656c6f
csgangsai06512-ue <csgangsai06512@gmail.com>
2026-04-13 15:50:01 +0900

    feat: user(회원정보) 베이스 코드

---
+++ backend/src/main/java/com/team01/backend/domain/user/entity/User.java
@@ -0,0 +1,32 @@
+package com.team01.backend.domain.user.entity;
+
+import com.team01.backend.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public User(String email, String password, String nickname, Role role) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+    }
+}
