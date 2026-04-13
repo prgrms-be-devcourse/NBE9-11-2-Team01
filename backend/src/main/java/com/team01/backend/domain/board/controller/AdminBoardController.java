0ac17ac87c0c0b44abdc0f7ffd65648e1249a8c4
YJin33 <89703649+YJin33@users.noreply.github.com>
2026-04-13 14:18:09 +0900

    refactor: boardController admin분리
    * feature:#2 게시판 생성 API 구현,테스트코드 작성
    
    * feat:#2 null, size 예외 처리
    
    * chore:#2 주석 추가 작성
    
    * chore:#2 boardController admin분리

--- backend/src/main/java/com/team01/backend/domain/board/controller/AdminBoardController.java
+++ backend/src/main/java/com/team01/backend/domain/board/controller/AdminBoardController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin/board")
-public class BoardController {
+public class AdminBoardController {
 
     private final BoardService boardService;
 
