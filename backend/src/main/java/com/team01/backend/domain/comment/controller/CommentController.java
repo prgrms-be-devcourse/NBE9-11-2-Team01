e6e1f89cdb2a1d5f95c2c962ad02e338888c0ffd
gyeongtaggim865-hue <gyeongtaggim865@gmail.com>
2026-04-14 11:07:54 +0900

    feat: 댓글 작성 테스트코드 Commnet InitData 생성 (#25)
    * fix : Comment API 명세서 변경
    
    * feat : 글 작성 예외 Test, BaseInitData 댓글 추가

--- backend/src/main/java/com/team01/backend/domain/comment/controller/CommentController.java
+++ backend/src/main/java/com/team01/backend/domain/comment/controller/CommentController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
 public class CommentController {
 
     private final CommentService commentService;
