4dcbfb4a4fecb9ae759964818fcdfd5876ac5543
Yujin <ujin3261@gmail.com>
2026-04-14 09:15:21 +0900

    chore: board api 경로 변경

--- backend/src/main/java/com/team01/backend/domain/board/controller/AdminBoardController.java
+++ backend/src/main/java/com/team01/backend/domain/board/controller/AdminBoardController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/admin/board")
+@RequestMapping("/admin/boards")
 public class AdminBoardController {
 
     private final BoardService boardService;
