e297c7bb4f07b7528158b13881580fb02bc11395
Yujin <ujin3261@gmail.com>
2026-04-16 10:26:31 +0900

    fix:카테고리 테이블에 boardId, name 중복 제약조건 추가

--- backend/src/main/java/com/team01/backend/domain/category/entity/Category.java
+++ backend/src/main/java/com/team01/backend/domain/category/entity/Category.java
@@ -2,12 +2,15 @@ package com.team01.backend.domain.category.entity;
 
 import com.team01.backend.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @Getter
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "boardId", "name" }) })
 public class Category extends BaseEntity {
     private Long boardId;
     private String name;
