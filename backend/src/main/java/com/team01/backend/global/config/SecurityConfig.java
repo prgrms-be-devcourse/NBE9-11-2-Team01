ee77d1b4529eab1e5bfeb408df7fc7d295a1c9a4
csgangsai06512-ue <csgangsai06512@gmail.com>
2026-04-22 15:31:46 +0900

    Update SecurityConfig.java

--- backend/src/main/java/com/team01/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/team01/backend/global/config/SecurityConfig.java
@@ -15,6 +15,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 import java.util.List;
 
