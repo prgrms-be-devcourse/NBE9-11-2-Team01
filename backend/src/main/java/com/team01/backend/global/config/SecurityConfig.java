acf3302f07e1061747d6c03a7c092aead8846b2c
csgangsai06512-ue <csgangsai06512@gmail.com>
2026-04-22 15:46:16 +0900

    Update SecurityConfig.java

--- backend/src/main/java/com/team01/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/team01/backend/global/config/SecurityConfig.java
@@ -15,10 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
 
 import java.util.List;
 
@@ -75,10 +72,7 @@ public class SecurityConfig {
         return source;
     }
 	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+
 
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
